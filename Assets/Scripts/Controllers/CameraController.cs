e0405233bc4e51ceac7302628df4996ac41e094c
vadwwxz <vadwwxz@gmail.com>
2023-03-24 22:53:16 +0200

    Created WalkGeneration, changed CameraController

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
     [SerializeField] private Transform player;
-    private float differenceByX = 1.63f, differenceByY = -3.65f; 
+    private float differenceByX = 1.72f, differenceByY = -3.75f; 
 
     private void Update() {
         Vector3 newPosition = new Vector3(player.position.x - differenceByX, player.position.y - differenceByY, transform.position.z);
---
+++ Assets/Scripts/Generators/WalkGeneration.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkGeneration : MonoBehaviour
+{
+    private List<GameObject> activeWalks = new List<GameObject>();
+    private float spawnPosition = 0;
+    private float walkLength = 16.8f;
+
+    [SerializeField] private GameObject[] walkPrefabs;
+    [SerializeField] private Transform player;
+
+    private void Update() {
+        if (player.position.y + walkLength >= spawnPosition) {
+            CreateWalk(Random.Range(0, walkPrefabs.Length));
+            if (player.position.y > 6) DeleteWalk();
+        }
+    }
+
+    private void CreateWalk(int walkIndex) {
+        Vector3 position = new Vector3(0, spawnPosition, 0);
+        GameObject nextWalk = Instantiate(walkPrefabs[walkIndex], position, Quaternion.identity);
+        activeWalks.Add(nextWalk);
+        spawnPosition += walkLength;
+    }
+
+    private void DeleteWalk() {
+        Destroy(activeWalks[0]);
+        activeWalks.RemoveAt(0);
+    }
+}
